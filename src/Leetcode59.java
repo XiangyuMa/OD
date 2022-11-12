49e760da264b7c10c1d59239cfcce0a17e399f63
maxiangyu <coderjing@163.com>
2022-11-12 22:38:23 +0800

    leetcode螺旋矩阵

---
+++ src/Leetcode59.java
@@ -0,0 +1,32 @@
+/**
+ * @author :马翔宇
+ * @description 给你一个正整数 n ，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。
+ * @date: 2022/11/12 21:25
+ */
+public class Leetcode59 {
+    public static void main(String[] args) {
+        int[][] arr = generateMatrix(3);
+    }
+    public static int[][] generateMatrix(int n) {
+        int maxNum = n * n;
+        int curNum = 1;
+        int[][] matrix = new int[n][n];
+        int row = 0, column = 0;
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; // 右下左上
+        int directionIndex = 0;
+        while (curNum <= maxNum) {
+            matrix[row][column] = curNum;
+            curNum++;
+            int nextRow = row + directions[directionIndex][0], nextColumn = column + directions[directionIndex][1];
+            if (nextRow < 0 || nextRow >= n || nextColumn < 0 || nextColumn >= n || matrix[nextRow][nextColumn] != 0) {
+                // 控制螺旋方向改变
+                directionIndex = (directionIndex + 1) % 4; // 顺时针旋转至下一个方向
+            }
+            row = row + directions[directionIndex][0];
+            column = column + directions[directionIndex][1];
+        }
+        return matrix;
+    }
+
+
+}
