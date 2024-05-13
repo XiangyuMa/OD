1acc12db70d2cc84b8f6d276fee5c2a97583b36b
maxiangyu <1056816866@qq.com>
2024-05-13 16:46:56 +0800

    for循环加二分查找实现有序三元组

--- src/H3.java
+++ src/H3.java
@@ -14,15 +14,24 @@ public class H3 {
         for(int i = 0;i<n;i++){
             arr[i] = scanner.nextInt();
         }
-        for(int i = 0;i < arr.length-2;i++){
-            for(int j = i+1;j < arr.length-1;j++){
-                for(int m = j+1;m < arr.length;m++){
-                    if(arr[i] + arr[j] + arr[m] == k){
-                        if(arr[i] == arr[j] || arr[i] == arr[m] || arr[j] == arr[m]){
-                            continue;
-                        }
-                        System.out.println(arr[i] + " " + arr[j] + " "+ arr[m]);
+        for(int i=0;i<arr.length-3;i++){
+            if(i>0 && arr[i]==arr[i-1]){
+                continue;
+            }
+            int left = i+1;
+            int right = arr.length -1;
+            int middleValue = k - arr[i];
+            while(left<right){
+                if(arr[left]+arr[right]<middleValue){
+                    left++;
+                }else if(arr[left]+arr[right]>middleValue){
+                    right--;
+                }else{
+                    if(arr[left]==0||arr[left]!=arr[left-1]&&arr[left]!=arr[right]){
+                        System.out.println(arr[i]+" "+arr[left]+" "+arr[right]);
                     }
+                    left++;
+                    right--;
                 }
             }
         }
